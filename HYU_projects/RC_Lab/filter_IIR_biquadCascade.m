e6202a1f4f689c9af58a34a5850b345f3aa74104
FezChoi <70895582+FezChoi@users.noreply.github.com>
2021-04-12 14:04:18 +0900

    Update filter_IIR_biquadCascade.m

--- HYU_projects/RC_Lab/filter_IIR_biquadCascade.m
+++ HYU_projects/RC_Lab/filter_IIR_biquadCascade.m
@@ -3,9 +3,8 @@ function [output ,output_p ] = filter_IIR_biquadCascade(data,filtercoef,scaleVal
 %   Detailed explanation goes here
 
 
-% filtercoef = [1,0,-1,1,-1.9632,0.9644;...
-%                    1,0,-1,1,-1.8501,0.8681];
-% scaleVals = [0.0602, 0.0602,1];
+% filtercoef = [];
+% scaleVals = [];
 
 % numCoefsStage = 6;
 for ii = 1:num_stage
@@ -17,15 +16,15 @@ for ii = 1:num_stage
     a2 = filtercoef(index_tmp ,6);
     scaleV = scaleVals(ii);
     
-%     output_p(ii) = scaleV*data - a1*output_p(ii+1) - a2*output_p(ii+2) ;
-%     y  = b0*output_p(ii) +b1*output_p(ii+1) +b2*output_p(ii+2);
+%     output_p(ii) = ;
+%     y  = ;
 %     
 %     output_p(ii +2) = output_p(ii+1);
 %     output_p(ii +1) = output_p(ii );
 %     data = y;
     
-    output_p(ii,end) = scaleV*data - a1*output_p(ii,end-1) - a2*output_p(ii,end- 2) ;
-    y  = b0*output_p(ii,end) +b1*output_p(ii,end- 1) +b2*output_p(ii,end- 2);
+    output_p(ii,end) = ;
+    y  = ;
     
     output_p(ii,end- 2)= output_p(ii,end- 1);
     output_p(ii,end-1) = output_p(ii,end);
