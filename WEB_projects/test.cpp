8a12a290032359a7853885fba2d0dff8b0643e52
FezChoi <70895582+FezChoi@users.noreply.github.com>
2021-06-01 21:08:22 +0900

    Add files via upload

---
+++ WEB_projects/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str = "hello yerin";
+    //cin >> str;
+    cout << "input is : " << str << endl;
+
+    return 0;
+}
